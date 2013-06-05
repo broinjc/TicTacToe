a9b68d7c19e44df6949b55a6f3f79019819b45f3
Nathan Nichols <broinjc@gmail.com>
2013-06-05 16:26:34 -0700

    Basic game play
    Works, but want to add features now

---
+++ ttt.py
@@ -0,0 +1,52 @@
+p1m = []    # player 1 list of moves
+p2m = []    # player 2 list of moves 
+b = [0,1,2, # the board
+     3,4,5,
+     6,7,8]
+
+def check(w, p):
+    j = 0
+    for i in w:
+        if i in p:
+            j += 1
+    if j == 3:
+        return True
+    else:
+        return False
+def checkWin(p):
+    wins = [[0,1,2],[3,4,5],[6,7,8],
+            [0,3,6],[1,4,7],[2,5,8],
+            [0,4,8],[2,4,6]]
+    for w in wins:
+        if(check(w,p)):
+            return True
+    return False
+
+def printBoard():
+# TODO make this print prettier!
+    print b[0:3]
+    print b[3:6]
+    print b[6:9]
+
+# TODO play against the computer (random or AI, easy or hard)
+while(True):
+# TODO D.R.Y. 
+    printBoard()
+# basic game play logic
+    p1 = int(raw_input("p1: "))
+    b[p1] = "x"
+    p1m.append(p1)
+    printBoard()
+    if(checkWin(p1m)):
+        print "\nPLAYER 1 WINS!"
+        break
+    p2 = int(raw_input("p2: "))
+    # TODO check if b[p2] is already a char
+    b[p2] = "o"
+    p1m.append(p2)
+    printBoard()
+    if(checkWin(p2m)):
+        print "\nPLAYER 2 WINS!"
+        break
+
+print "THANKS FOR PLAYING\n"
